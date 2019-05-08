3c16703e8dac1609fd32182983c9a0a8641e446a
Thomas499 <38442058+Thomas499@users.noreply.github.com>
2019-05-08 11:48:22 -0700

    Will compile now
    updated dependencies, sdk version to 28, updated gradle

--- app/src/main/java/com/resocoder/timertutorial/SettingsActivity.kt
+++ app/src/main/java/com/resocoder/timertutorial/SettingsActivity.kt
@@ -1,6 +1,6 @@
 package com.resocoder.timertutorial
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_timer.*
 
--- app/src/main/java/com/resocoder/timertutorial/util/NotificationUtil.kt
+++ app/src/main/java/com/resocoder/timertutorial/util/NotificationUtil.kt
@@ -11,7 +11,7 @@ import android.graphics.Color
 import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Build
-import android.support.v4.app.NotificationCompat
+import androidx.core.app.NotificationCompat
 import com.resocoder.timertutorial.AppConstants
 import com.resocoder.timertutorial.R
 import com.resocoder.timertutorial.TimerActivity
